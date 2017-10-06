31f2d638486e57a5ff80d9611a23c01109292e1f
Svetlana <svetlana.i.tch@gmail.com>
2017-10-06 03:19:02 +0200

    Отображение в таблице всех заказов

---
+++ CarService/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CarService.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    { 
+        public List<OrderExtended> OrdersAll { get; private set; }
+
+        public MainViewModel()
+        {
+            OrdersAll = QueriesDB.GetOrdersExtended();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ CarService/ViewModel/QueriesDB.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarService.Model;
+
+namespace CarService.ViewModel
+{
+     class OrderExtended
+    {
+        public Int32 IdOrder { get; set; }
+        public string CarBrand { get; set; }
+        public string CarModel { get; set; }
+        public Int16 ReleaseYear { get; set; }
+        public string TransmissionType { get; set; }
+        public Int16 EnginePower { get; set; }
+        public string NameOperation { get; set; }
+        public DateTime BeginTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public Decimal Price { get; set; }
+    }
+    class QueriesDB
+    {           
+
+        public static List<OrderExtended> GetOrdersExtended()
+        {
+            List<OrderExtended> res;
+
+            using (dbEntities db = new dbEntities())
+            {
+                var result = from or in db.OrderSet
+                             join c in db.CarSet on or.CarId equals c.IdCar
+                             join op in db.OperationSet on or.OperationId equals op.IdOperation
+                             select new OrderExtended
+                             {
+                                 IdOrder = or.IdOrder,
+                                 CarBrand = c.CarBrand,
+                                 CarModel = c.CarModel,
+                                 ReleaseYear = (Int16)c.ReleaseYear,
+                                 TransmissionType = (string)c.TransmissionType,
+                                 EnginePower = (Int16)c.EnginePower,
+                                 BeginTime = (DateTime)or.BeginTime,
+                                 //EndTime = (DateTime)or.EndTime,
+                                 NameOperation = op.NameOperation,
+                                 Price = (Decimal)op.Price
+                             };
+                res = result.ToList();
+            }
+
+            return res;
+        }
+    }
+}
